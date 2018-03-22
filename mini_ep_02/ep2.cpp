03e06fef7a461cd521280431cbca3ce355c4867a
Bruno B. Scholl <bruno.scholl@usp.br>
2018-03-21 22:30:02 -0300

    [mini_ep_02] Implement most part of the frog class.

--- mini_ep_02/ep2.cpp
+++ mini_ep_02/ep2.cpp
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -7,7 +8,7 @@
  */
 class Toad
 {
-public:
+  public:
     // Constructor
     Toad(int starting_stone, int *stones);
 
@@ -17,7 +18,7 @@ public:
     // Waits thread
     void wait();
 
-private:
+  private:
     // Position
     int position;
 
@@ -31,8 +32,8 @@ private:
     pthread_t thread_handler;
 };
 
-Toad::Toad(int starting_stone, int *stones) :
-    position(starting_stone), stones(stones)
+Toad::Toad(int starting_stone, int *stones)
+    : position(starting_stone), stones(stones)
 {
     stones[position] = this;
 }
@@ -42,30 +43,102 @@ void Toad::start_thread()
     pthread_create(&this->thread_handler, nullptr, &Toad::thread, this);
 }
 
-void Toad::wait()
-{
-    pthread_join(this->thread_handler, nullptr);
-}
+void Toad::wait() { pthread_join(this->thread_handler, nullptr); }
 
 void *Toad::thread(void *instance)
 {
-    Toad &toad = *((Toad*) instance);
+    Toad &toad = *((Toad *)instance);
 
     for (int i = 0; i < 10; i++)
-      printf("Yay %d\n", toad.position);
+        printf("Yay %d\n", toad.position);
 }
 
 /**
- * Class representing a frog
+ * Class representing a frog (start on the left and go to the right)
  */
 class Frog
 {
+  public:
+    // Constructor
+    Frog(int starting_stone, int *stones);
+
+    // Waits Thread to finalise
+    void wait();
+
+  private:
+    // Frog's position in the stone array
+    int position;
+
+    // Thread handler
+    pthread_t thread_handler;
+
+    // Pointer to the stone array
+    int *stones;
+
+    /**
+     * Function passed to the thread initializer to be the main thread function
+     * Receives: a frog instance (since it has to be static), the barrier to
+     * sync the start and the semaphore to stomize actions in the stone array
+     */
+    static void *thread(void *frog_instance, pthread_barrier_t *start,
+                        sem_t *stones_semaphore);
+
+    // Function called wehn the frog can jump
+    int jump();
+
+    // Funtion that evaluates if Frog can jump
+    bool can_jump();
 };
 
+Frog::Frog(int starting_stone, int *stones)
+    : position(starting_stone), stones(stones)
+{
+    // TODO: We have to find a way to better represent this
+    stones[position] = this;
+    pthread_create(&this->thread_handler, nullptr, &Frog::thread, this);
+}
+
+void *Frog::thread(void *frog_instance, pthread_barrier_t *start,
+                   sem_t *stones_semaphore)
+{
+    Frog *instance = (Frog *)frog_instance;
+    // Waits the program to start
+    pthread_barrier_wait(start);
+
+    sem_wait(stones_semaphore);
+    if (instance->can_jump()) instance->jump();
+    else 
+        cant_jump_counter++;
+    sem_post(stones_semaphore);
+}
+
+int Frog::jump()
+{
+    stones[position + 1] = this;
+    stones[position]     = 0;
+}
+
+bool Frog::can_jump()
+{
+    if (this->stones[position + 1] == 0)
+        return true;
+    else if (stones[position + 2] == 0)
+        return true;
+    else
+        return false;
+}
+
+void Frog::wait() { pthread_join(this->thread_handler, nullptr); }
+
 Toad *toad[500];
+int *stones[1000];
+sem_t *stones_semaphore;
 
 int main(int argc, char *argv[])
 {
+    // Initialize the semaphre to atomize the frog jumps
+    sem_init(stones_semaphore, 0, 1);
+
     for (int i = 0; i < 500; i++)
         toad[i] = new Toad(i, nullptr);
     for (int i = 0; i < 500; i++)
