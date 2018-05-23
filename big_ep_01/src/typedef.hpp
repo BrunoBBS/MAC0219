5ca1123f62ab7a2f3a980c2a176eef1abde65a99
Victor Seiji Hariki <victorseijih@gmail.com>
2018-05-23 17:15:00 -0300

    [big_ep_01] Typedef atualizado com header da matriz nova

--- big_ep_01/src/typedef.hpp
+++ big_ep_01/src/typedef.hpp
@@ -1,9 +1,21 @@
 #ifndef __TYPEDEF__HPP__
 
-#include <vector>
-
 using namespace std;
 
-typedef vector<vector<double>> mat;
+// Class representing a matrix
+class Matrix {
+public:
+    Matrix(uint64_t rows, uint64_t cols);
+
+    uint64_t rows() const;
+    uint64_t cols() const;
+
+    double*& operator[] (uint64_t row);
+
+private:
+    double **matrix;
+};
+
+typedef Matrix mat;
 
 #endif
