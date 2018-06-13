393aa18cf5d989a9cf9673646b0868a925777262
Victor Seiji Hariki <victorseijih@gmail.com>
2018-06-13 18:18:46 -0300

    Now reading file properly

--- big_ep_02/src/main.cpp
+++ big_ep_02/src/main.cpp
@@ -17,17 +17,16 @@ typedef vector<vector<int32_t>> mat_t;
  * @param matrix Mtartix to fill.
  * @return a struct matrices.
  */
-void load_matrices(ifstream &in_file, int num_mat, mat_t &matrix)
+void load_matrices(ifstream &in_file, int32_t num_mat, mat_t &matrix)
 {
+    std::string ast;
     for (int mat = 0; mat < num_mat; mat++)
     {
-        // ignores line of *
-        in_file.ignore();
+        in_file >> ast;
         // Reads actual numbers from lines
         in_file >> matrix[0][mat] >> matrix[1][mat] >> matrix[2][mat];
-        cout << matrix[0][mat] << " " << matrix[1][mat];
-        in_file >> matrix[4][mat] >> matrix[5][mat] >> matrix[6][mat];
-        in_file >> matrix[7][mat] >> matrix[8][mat] >> matrix[8][mat];
+        in_file >> matrix[3][mat] >> matrix[4][mat] >> matrix[5][mat];
+        in_file >> matrix[6][mat] >> matrix[7][mat] >> matrix[8][mat];
     }
 }
 
@@ -35,14 +34,20 @@ int main(int agrc, char *argv[])
 {
     std::ifstream in_file;
     in_file.open(argv[1]);
-    if (!in_file.is_open())
+    if (!in_file)
         error(format("File '%s' couldn't be opened!", argv[1]));
+
+    
     int32_t num_mat;
     in_file >> num_mat;
+
+    
     mat_t mat(9);
     for (int i = 0; i < 9; i++)
         mat[i].resize(num_mat);
+
     load_matrices(in_file, num_mat, mat);
+    
     for (auto item : mat)
     {
         cout << "[";
@@ -52,4 +57,5 @@ int main(int agrc, char *argv[])
         }
         cout << "]" << endl;
     }
+    in_file.close();
 }
