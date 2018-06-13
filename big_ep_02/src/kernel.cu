8b2fcd02a803dc1f7d8d36af74aedb3c5e801a64
Bruno B Scholl <bruno.scholl@usp.br>
2018-06-13 18:12:22 -0300

    [big_ep_2] Create beta kernel code.

--- big_ep_02/src/kernel.cu
+++ big_ep_02/src/kernel.cu
@@ -1,6 +1,32 @@
 __global__
-void reduce_min(int mat_num, int )
+void reduce_min(int n_mat, int* g_values)
 {
-    int i = blockIdx*blockDim + threadIdx.x;
-    if (i % 2 == 0) 
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    // Local(bock) thread id
+    unsigned int lid = threadIdx.x;                                              
+    // Global (across blocks) thread id
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;                        
+
+    // Reference to this thread block
+    cg::thread_block this_block = cg::this_thread_block();                              
+    int *local_data = SharedMemory<int>();                                                
+
+    local_data[lid] = (i < n_mat) ? g_values[i] : g_values[0];                                       
+
+    // wait untill all threads updated the array values
+    cg::sync(this_block);                                                               
+
+    //reduce the vector
+    for (unsigned int s=blockDim.x/2; s>0; s>>=1)                                
+    {                                                                            
+        if (lid < s)                                                             
+        {                                                                           
+            local_data[lid] += min(local_data[lid], local_data[lid + s]);                                        
+        }                                                                        
+                                                                                    
+        cg::sync(this_block);                                                              
+    }                                                                               
+    // Write results to global memory
+    if (lid == 0) g_values[blockIdx.x * blockDim.x] = local_data[0];                                   
 }
