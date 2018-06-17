c9973fb48e3b78503a39b6641d78037e8084138c
Bruno B Scholl <bruno.scholl@usp.br>
2018-06-17 16:19:54 -0300

    [big_ep_2] Change thread handling to double size. Make so that a thread looks to 2 values in the first iteration.

--- big_ep_02/include/kernel.cuh
+++ big_ep_02/include/kernel.cuh
@@ -2,24 +2,23 @@
 #define KERNEL_CU
 #include <cuda_runtime.h>
 
-struct SharedMemory                                                              
-{                                                                                
-    __device__ inline operator int *()                                       
-    {                                                                            
-        extern __shared__ int __smem[];                                             
-        return __smem;                                                         
-    }                                                                               
-                                                                                 
-    __device__ inline operator const int *() const                                 
-    {                                                                            
-        extern __shared__ int __smem[];                                                                                                                                                                              
-        return __smem;                                                      
-    }                                                                            
+struct SharedMemory
+{
+    __device__ inline operator int *()
+    {
+        extern __shared__ int __smem[];
+        return __smem;
+    }
+
+    __device__ inline operator const int *() const
+    {
+        extern __shared__ int __smem[];
+        return __smem;
+    }
 };
 
-__global__
-void reduce_min(int n_mat, int* g_values);
+__global__ void reduce_min(int n_mat, int *g_values);
 
-void reduce(int num_mat, void* device_array);
+void reduce(int num_mat, void *device_array);
 
 #endif
--- big_ep_02/src/kernel.cu
+++ big_ep_02/src/kernel.cu
@@ -4,26 +4,26 @@
 
 using namespace cooperative_groups;
 
-__global__
-void reduce_min(int n_mat, int* g_values)
+__global__ void reduce_min(int n_mat, int* g_values)
 {
     // Local(block) thread id
     unsigned int lid = threadIdx.x;
 
     // Global (across blocks) thread id
-    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int i = 2 * blockIdx.x * blockDim.x + threadIdx.x;
 
     // Reference to this thread block
     thread_block this_block = this_thread_block();
     int *local_data = SharedMemory();
 
     local_data[lid] = (i < n_mat) ? g_values[i] : g_values[0];
+    local_data[lid + blockDim.x] = (i + blockDim.x < n_mat) ? g_values[i + blockDim.x] : g_values[0];
 
-    // wait untill all threads updated the array values
+    // wait until all threads updated the array values
     sync(this_block);
 
     //reduce the vector
-    for (unsigned int s=blockDim.x/2; s>0; s>>=1)
+    for (unsigned int s = blockDim.x; s > 0; s >>= 1)
     {
         if (lid < s)
             local_data[lid] += min(local_data[lid], local_data[lid + s]);
@@ -31,17 +31,22 @@ void reduce_min(int n_mat, int* g_values)
         sync(this_block);
     }
     // Write results to global memory
-    if (lid == 0) g_values[blockIdx.x * blockDim.x] = local_data[0];
+    if (lid == 0) g_values[2 * blockIdx.x * blockDim.x] = local_data[0];
 }
 
 void reduce(int num_mat, void* device_array)
 {
+    // Set the block size
     const int32_t block_size = 1024;
 
     int elements = num_mat;
-    do {
+
+    do 
+    {
+        // Calculates number of blocks sized block_size based on number of elements
         int32_t num_blocks = (elements / (block_size * 2)) + (elements % (block_size * 2) != 0);
 
+        // Calls the kernel for each position of the matrix (piece of the vector)
         for (int item = 0; item < 9; item++)
             reduce_min<<<num_blocks, block_size>>>(elements, ((int*) device_array) + item * elements);
 
