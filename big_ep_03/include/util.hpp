d3b89bca2fc2c007b7f3a2cdb5f0928fd7a93123
Victor Seiji Hariki <victorseijih@gmail.com>
2018-06-27 14:55:41 -0300

    [big_ep_03] Moved parsing to util

--- big_ep_03/include/util.hpp
+++ big_ep_03/include/util.hpp
@@ -13,6 +13,10 @@ inline double f(uint64_t M, uint64_t k, double x)
     return (sin((2 * M + 1) * M_PI * x) * cos(2 * M_PI * k * x))/sin(M_PI * x);
 }
 
+// Converts strings to integers
+void read(uint64_t &v, std::string v_str, std::string name);
+void read(int64_t  &v, std::string v_str, std::string name);
+
 // Returns printf-like formatted std::string
 std::string format(std::string pattern, ...);
 
